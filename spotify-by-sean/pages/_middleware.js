3f1d31ffefbe399723fd3a15f62948af4ba5e994
Sean Hughes <sneakerheadsean@gmail.com>
2021-11-28 10:23:49 -0500

    Logout / Login Completed
    Added middleware to check if user is logged in and has a token. If not, they are redirected to login.

---
+++ spotify-by-sean/pages/_middleware.js
@@ -0,0 +1,18 @@
+import { getToken } from 'next-auth/jwt'
+import { NextResponse } from 'next/server'
+
+export async function middleware(req) {
+  // Token will exist if user is logged in...
+  const token = await getToken({ req, secret: process.env.JWT_SECRET })
+
+  const { pathname } = req.nextUrl
+  // Allow the request if the following is true...
+  //  A. The token exists...
+  if (pathname.includes('/api/auth') || token) {
+    return NextResponse.next()
+  }
+  // If not, redirect them to the login page...
+  if (!token && pathname !== '/login') {
+      return NextResponse.redirect("/login")
+  }
+}
